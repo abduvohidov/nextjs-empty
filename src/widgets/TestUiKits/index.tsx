5fe15eb24555bf98df4054c1d2a9f1eed328b3b8
Abdullah <abduvohidov.abdullokh@gmail.com>
2025-03-19 08:57:30 +0500

    feat: add scale action to button

--- src/widgets/TestUiKits/index.tsx
+++ src/widgets/TestUiKits/index.tsx
@@ -1,21 +1,22 @@
-import { Button } from "@/shared/ui/button";
-import { FC } from "react";
+"use client";
 
-export const TestUikits: FC = () => {
+import React from "react";
+import { Button } from "@/shared/ui/button";
+
+export const TestUikits: React.FC = () => {
   return (
-    <div className="m-10 flex gap-4">
-      <Button className="mt-5" variant="default">
-        Click me
-      </Button>
-      <Button className="mt-5" variant="destructive">
-        Click me
-      </Button>
-      <Button size="lg" className="mt-5" variant="outline">
-        Click me
-      </Button>
-      <Button size="sm" className="mt-5" variant="link">
-        Click me
-      </Button>
+    <div className="container mx-auto mt-20">
+      <div className="flex justify-center gap-4">
+        <Button size="lg" className="mt-5" variant="default">
+          Click me
+        </Button>
+        <Button size="default" className="mt-5" variant="destructive">
+          Click me
+        </Button>
+        <Button size="sm" className="mt-5" variant="outline">
+          Click me
+        </Button>
+      </div>
     </div>
   );
 };
