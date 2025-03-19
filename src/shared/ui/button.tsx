c10132b786ee58f6af74ea585423c6b522a0542e
Abdullah <abduvohidov.abdullokh@gmail.com>
2025-03-19 10:11:49 +0500

    refactor: change utils logic

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import { motion } from "motion/react";
 
 import { cn } from "@/shared/lib/utils";
 
